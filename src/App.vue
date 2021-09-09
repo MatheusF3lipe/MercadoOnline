55b8091e1c20bda95cb3f3eff72eef5381975204
Matheus Zali <66894179+ZXL7@users.noreply.github.com>
2021-09-08 21:38:00 -0300

    header

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,37 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+  <v-app id="inspire">
+    <v-app-bar app shrink-on-scroll class="red white--text">
+      <v-toolbar-title
+        >Supermercado Online
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+        <v-btn icon>
+          <v-icon class="white--text">mdi-account</v-icon>
+        </v-btn>
 
-      <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon class="white--text">mdi-heart</v-icon>
+        </v-btn>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+        <v-btn icon>
+          <v-icon class="white--text">mdi-cart</v-icon>
+        </v-btn>
+      </v-toolbar-title>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <v-container>
+        <v-row>
+          <v-col v-for="n in 24" :key="n" cols="4">
+            <v-card height="200"></v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+  //
 };
 </script>
